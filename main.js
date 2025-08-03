ee36358d9e228eed5284c1b21ed7c4c0341dce8b
Zhussup <119820180+Zhussup@users.noreply.github.com>
2025-08-03 23:45:43 +0500

    Update and rename main.js to checklist.txt

--- main.js
+++
@@ -1,9 +0,0 @@
-function addTask(){
-    const input = document.getElementById("taskText");
-    const text = input.value;
-    const taskList = document.getElementById("taskList");
-    const taskItem = document.createElement("div");
-    taskItem.textContent = text;
-    taskList.appendChild(taskItem);
-
-}
